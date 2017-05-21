5b7ea975dfc3c9a74ed7662d95246ab8f686de70
FMFluke <lutheus_phidelra@hotmail.com>
2017-05-21 17:09:01 +0700

    Start working on Move script, not much progress though. Need to look for more information on this first.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	Vector3 pos;
+	float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow) && transform.position == pos) { //Left arrow
+			pos += Vector3.left;
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && transform.position == pos) { //Right arrow
+			pos += Vector3.right;
+		}
+		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
+	}
+
+}
