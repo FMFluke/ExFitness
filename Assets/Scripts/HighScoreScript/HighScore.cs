f1eeaebb9d8b6b71b36c3948d407088a353f6c17
Pepomanz <pepoman1@gmail.com>
2017-05-23 22:56:26 +0700

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/HighScoreScript/HighScore.cs
+++ Assets/Scripts/HighScoreScript/HighScore.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
 
-class HighScore  {
+class HighScore:IComparable<HighScore> {
     public int Score { get; set; }
     public string Name { get; set; }
     public DateTime Date { get; set; }
@@ -14,5 +14,16 @@ class HighScore  {
         this.Score = score;
         this.ID = id;
         this.Name = name;
+    }
+
+    public int CompareTo(HighScore other)
+    {
+        if (other.Score < this.Score)
+            return -1;
+        else if (other.Score > this.Score)
+            return 1;
+        else
+            return 0;
+        //throw new NotImplementedException();
     }
 }
