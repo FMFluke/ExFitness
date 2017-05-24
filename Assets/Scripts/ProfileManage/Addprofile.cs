0931c19bc25acf3e6e40cd4c0dc6c49780cdfa70
Pepomanz <pepoman1@gmail.com>
2017-05-25 04:02:07 +0700

    Update Add Profile Complete

--- Assets/Scripts/ProfileManage/Addprofile.cs
+++ Assets/Scripts/ProfileManage/Addprofile.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Addprofile : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text EnterWeight;
+    public Text EnterHeight;
+    public Text EnterName;
+    public Text EnterGender;
+    private string connectionString;
+    public void InsertProfile(string name,int weight,int height,string gender)
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/Exfitness.sqlite";
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+            using (IDbCommand cmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("INSERT INTO PLAYER (Name,Weight,Height,Gender) VALUES (\"{0}\",{1},{2},\"{3}\")",name,weight,height,gender) ;
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteScalar();
+                dbConnection.Close();
+            }
+        }
+    }
+    public void Enter()
+    {
+        int Weight = int.Parse(EnterWeight.text);
+        int Height = int.Parse(EnterHeight.text);
+        InsertProfile(EnterName.text, Weight, Height, EnterGender.text);
+    }
 }
